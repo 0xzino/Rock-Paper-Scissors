ea8aaaaf9692b88a97a143929136596ec2969fb6
0xzino <nizarcarjoun@gmail.com>
2025-07-28 09:22:21 +0800

    Added function for computer generated decision

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
 console.log("Hello World!");
+
+function getComputerChoice() {
+    // Randomly selects rock, paper, or scissors
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+console.log(getComputerChoice());
