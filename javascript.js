fb22a4925af6f83c9f0efb10f90410c1354a2806
0xzino <nizarcarjoun@gmail.com>
2025-07-31 11:33:49 +0800

    Added 2 global variables, humanScore and computerScore

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@ console.log("Hello World!");
     
 const choices = ["rock", "paper", "scissors"];
 let winCon = {"rock":"scissors", "paper":"scissors", "scissors":"paper"}
+let humanScore = 0
+let computerScore = 0
 
 function getComputerChoice() { // Randomly selects rock, paper, or scissors
     const randomIndex = Math.floor(Math.random() * choices.length);
@@ -13,4 +15,4 @@ function getHumanChoice() { // Prompts user for rock, paper, or scissors
 }
 
 console.log(getComputerChoice());
-console.log(getHumanChoice())   
+console.log(getHumanChoice());
